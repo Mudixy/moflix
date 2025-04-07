3a40e20fde5434502559f6084598c355aab8ab7c
Mudixy <mfxplaymobil@gmail.com>
2025-04-07 17:46:28 +0200

    0.0.3 - Nouveaux programmes

--- series-data.js
+++ series-data.js
@@ -72,6 +72,94 @@ const series = [
         ],
         featured: false,
     },
+    { // HPI.
+      id: 112738,
+      title: "HPI",
+      year: 2021,
+      creator: "Alice Chegaray-Breugnot, Stéphane Carrié, Nicolas Jean",
+      rating: 7.5,
+      categories: ["comédie", "mystère", "crime", "familial"],
+      poster:
+        "https://image.tmdb.org/t/p/original/ucUNlxj8sSHy9ukJPxtEDyUMBoo.jpg",
+      backdrop:
+        "https://image.tmdb.org/t/p/original/dhNDdTXykscNFKnEqsMzMQuak3o.jpg",
+      description:
+        "Morgane, 38 ans, 3 enfants, 2 ex, 5 crédits, 160 de QI et une bonne dose d’insoumission, va voir son destin de femme de ménage chamboulé lorsque ses capacités hors norme sont repérées par la police qui lui propose un poste de consultante. Problème : Morgane déteste les flics !",
+      trailer: "",
+      seasons: [
+        {
+          number: 1,
+          year: 2021,
+          episodes: [
+              { 
+              number: 1, 
+              title: "Vents d'Ouest", 
+              duration: "52 min", 
+              description: "À la DIPJ de Lille, l’enquête sur le meurtre d’Antoine Levasseur, retrouvé assassiné chez lui est presque résolue. Sa femme Jeanne est portée disparue et il ne fait presque aucun doute qu’elle est la principale suspecte. Tombée par hasard sur le dossier du meurtre, Morgane Alvaro, la femme de ménage du commissariat au QI hors norme n’est pas de cet avis : pour elle, Jeanne est aussi une victime, et elle ne se prive pas de faire savoir aux enquêteurs qu’ils font fausse route ! Bien obligés d’admettre leur erreur et le talent indéniable de cette civile, Morgane est engagée comme consultante sur l’affaire ! Le très pointilleux commandant Karadec se voit obligé de collaborer avec cette mère célibataire au tempérament de feu qui a un sérieux problème avec l’autorité…", 
+              thumbnail: "https://image.tmdb.org/t/p/original/jtylBlccGhnEkRehLaxlfVbZwHW.jpg", 
+              videoUrl: "https://uqload.net/embed-c2wezfz5vlqj.html", 
+              }, 
+              { 
+              number: 2, 
+              title: "Coutumes Malgaches", 
+              duration: "52 min", 
+              description: "Benjamin Winckler est retrouvé noyé dans la baignoire d’une chambre d’hôtel apparemment fermée de l’intérieur ! Et Le mystère ne fait que s’épaissir quand les enquêteurs apprennent que la victime avait usurpé l’identité d’un homme, lui, bien vivant ! La finesse de Morgane ne sera pas de trop pour résoudre cette enquête. Car même si Karadec a toujours du mal avec ses frasques, elle a, en effet, accepté de rejoindre l’équipe en échange de la réouverture d’une enquête sur la disparition de Romain, son ex petit ami et père de sa fille aînée, Théa.", 
+              thumbnail: "https://image.tmdb.org/t/p/original/wVZrKZPJ9rLyyFsgmoMeLUArOUB.jpg", 
+              videoUrl: "https://nathanfromsubject.com/e/e9gnea9burpp", 
+              }, 
+              { 
+              number: 3, 
+              title: "Colin-maillard", 
+              duration: "52 min", 
+              description: "L’enlèvement de Juliette et Laura, deux petites filles de 8 et 5 ans, concentre les efforts de l’équipe. Leur père, Franck Sorin, semble être le suspect idéal. Homme isolé et apparemment instable, il était de plus en guerre ouverte avec le grand-père maternel des deux petites, Arnaud Grandgeon, riche propriétaire d’une grande chaîne d’hôtels de luxe. Mais Morgane en doute. Son intuition va être confirmée lorsque les enquêteurs découvrent le corps de Franck, enterré dans son jardin ! Mais alors, qui a pu enlever les deux petites ? Morgane va tout faire pour aider Florence, la mère des deux petites, à retrouver ses enfants. Dans cette course contre la montre, Morgane devra garder la tête sur les épaules alors même qu’elle vient d’apprendre que sa mère à elle, Agnès, a peut-être un rapport avec la disparition de Romain.", 
+              thumbnail: "https://image.tmdb.org/t/p/original/e8rdgQLdQgj13BePMokJ2ehbZSv.jpg", 
+              videoUrl: "https://nathanfromsubject.com/e/wnipfosqv9am", 
+              },
+              { 
+              number: 4, 
+              title: "Phyllobates terribilis", 
+              duration: "52 min", 
+              description: "Alors que l’enquête sur la disparition de Romain est au point mort, Fanny Marini, une vétérinaire spécialisée dans les animaux exotiques, est retrouvée sans vie dans son cabinet. Une mort tout à fait naturelle pour la police… Mais un meurtre prémédité pour Morgane, qui prouve que la victime a été empoisonnée. Femme sans histoire et adorée de tous ses amis, les suspects ne sont, au premier abord, pas légion. Mais entre un ex-petit copain fraîchement sorti de prison et une affaire de trafic d’animaux exotiques, la gentille vétérinaire cachait bien son jeu.", 
+              thumbnail: "https://image.tmdb.org/t/p/original/9hQtDCHdQLoWBAls0gqJhM99X8e.jpg", 
+              videoUrl: "https://nathanfromsubject.com/e/iiujp6wktptg", 
+              },
+              { 
+              number: 5, 
+              title: "Tel maître, tel chien", 
+              duration: "52 min", 
+              description: "Avec la confirmation de la mort naturelle de Romain, Morgane doit vivre son deuil à rebours. C'est alors qu'Émilien Kerr, est retrouvé dans un parking, étouffé à l'aide d'un sac plastique. Il est envoyé à l'hôpital en catastrophe, mais les intentions de l'agresseur et le manque de suspects crédibles brouillent les pistes. A-t-on vraiment essayé de tuer Émilien Kerr ? Morgane se le demande... Et si ce n'est pas le cas, pourquoi se donner tant de mal pour l'envoyer à l'hôpital toujours vivant ? Morgane et Karadec vont tenter de le découvrir en fouillant dans le passé d'une victime aux portes de la mort.", 
+              thumbnail: "https://image.tmdb.org/t/p/original/mRhWpjjkXb3kbZQ15r6Z2I6coKq.jpg", 
+              videoUrl: "https://nathanfromsubject.com/e/sxpvlpzpq4cd",
+              },
+              { 
+              number: 6, 
+              title: "Hep et Soja", 
+              duration: "52 min", 
+              description: "Libérée du fantôme de Romain, Morgane a bien pris ses marques dans la police. Et même trop pour Karadec, dont elle parasite même les rêves. Il doit pourtant se concentrer sur le meurtre d’Alex Viniali, retrouvé assassiné dans un vestiaire de football. Et c’est peut-être le foot justement qui a conduit à la mort ce père de famille exemplaire. Car Alex Viniali faisait partie d’un groupe de supporters lillois particulièrement violents… Accompagnée de sa fille, Théa, venue faire un stage à la DIPJ, Morgane se réjouit de pouvoir mettre son nez dans un univers trop souvent réservé aux hommes, celui des supporters ! Karadec un peu moins…", 
+              thumbnail: "https://image.tmdb.org/t/p/original/j6fbUcuwxoDUEkl8c0J1LIRBi6P.jpg", 
+              videoUrl: "https://do7go.com/e/11t0du8irprb",
+              },
+              { 
+              number: 7, 
+              title: "Cocktail molotov", 
+              duration: "52 min", 
+              description: "Morgane peut enfin souffler ! Ses enfants en vacances, elle va avoir le temps de se la couler douce… Et (bonne ?) nouvelle, le meurtre sur lequel elle doit aider la police a eu lieu au bord de la mer. La scène de crime est pourtant loin d’être paradisiaque et le corps d’Axel Maertens, directeur d’un club de kitesurf, a été traîné sur la plage par un char à voile. Alors que tout désigne Jonathan, témoin du meurtre, Morgane, qui doute de la culpabilité du jeune homme, va tenter de prouver son innocence ! Et pour une fois, Karadec est prêt à la suivre… Quitte à passer sous le radar de la hiérarchie.", 
+              thumbnail: "https://image.tmdb.org/t/p/original/e2ANpqUZdnHjmzACuXuytu2pfK7.jpg", 
+              videoUrl: "https://nathanfromsubject.com/e/9jndx818zs1q",
+              },
+              { 
+              number: 8, 
+              title: "Homme de peu de foi", 
+              duration: "52 min", 
+              description: "C’est dans un climat trouble que Morgane et Karadec sont envoyés sur une nouvelle affaire. Pauline Garcia, une patineuse est retrouvée morte renversée par sa propre voiture ! Par chance, les enquêteurs ont un témoin qui aurait croisé le meurtrier… Le seul problème c’est que celle-ci ne peut pas leur donner de description visuelle : elle est aveugle. Alors que les sentiments de Karadec se font de plus en plus confus pour sa consultante et que Morgane semble se rapprocher de son deuxième ex, Ludo, cette dernière enquête va mettre les talents de nos enquêteurs à rude épreuve.", 
+              thumbnail: "https://image.tmdb.org/t/p/original/fAenJF6q5DarBmAGg8uDUTQYW1s.jpg", 
+              videoUrl: "https://nathanfromsubject.com/e/eivkcninsrvx",
+              },
+          ]
+        },
+      ],
+      featured: false,
+  },
   ]
   
   // Fonction pour obtenir une série par son ID
